b7277b784e5d97cf41cb8160a50319a302f55952
Aquib12345 <aquib.rizi17@gmail.com>
2022-09-17 02:36:29 +0530

    my project

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+	WebDriver driver = null;
+
+	public LoginPage(WebDriver driver) 
+	{
+		
+	    //PageFactory.initElements(driver, this);
+		System.out.println("Cons Called");
+		this.driver = driver;
+		
+	}
+
+	By user = By.name("username");
+
+	By pass = By.name("password");
+
+	By loginButton = By.xpath("//button[contains(@class,'button')]");
+
+	public DashboardPage loginToApplication(String uname, String password) 
+	{
+		driver.findElement(user).sendKeys(uname);
+		driver.findElement(pass).sendKeys(password);
+		driver.findElement(loginButton).click();
+		
+		return new DashboardPage();
+		
+	}
+
+}
+
